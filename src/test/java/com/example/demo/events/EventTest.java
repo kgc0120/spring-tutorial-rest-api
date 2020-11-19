15d1295624ac111f616a559c8e584dacb89255bf
kgc0120 <senru0508@gmail.com>
2020-11-20 00:35:18 +0900

    Junit useing Parameters

--- src/test/java/com/example/demo/events/EventTest.java
+++ src/test/java/com/example/demo/events/EventTest.java
@@ -3,7 +3,12 @@ package com.example.demo.events;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
 
 	@Test
@@ -29,12 +34,13 @@ public class EventTest {
 		
 	}
 	
-	@Test
-	public void testFree() {
+	@org.junit.Test
+	@Parameters// (method = "parametersForTestFree") method 이름과 test 메소드 이름 앞에 parameters 제외하고 같으면 생략 가능
+	public void testFree(int basePrice, int maxPrice, boolean isFree) {
 		// Given 이런 상태에서
 		Event event = Event.builder()
-				.basePrice(0)
-				.maxPrice(0)
+				.basePrice(basePrice)
+				.maxPrice(maxPrice)
 				.build();
 		
 		// When 이런일이 벌어지면
@@ -42,41 +48,24 @@ public class EventTest {
 		
 		
 		// Then 이렇게 된다
-		assertThat(event.isFree()).isTrue();
-		
-		// Given 이런 상태에서
-		event = Event.builder()
-				.basePrice(100)
-				.maxPrice(0)
-				.build();
-		
-		// When 이런일이 벌어지면
-		event.update();
-		
-		
-		// Then 이렇게 된다
-		assertThat(event.isFree()).isFalse();
-		
-		// Given 이런 상태에서
-		event = Event.builder()
-				.basePrice(0)
-				.maxPrice(100)
-				.build();
-		
-		// When 이런일이 벌어지면
-		event.update();
-		
-		
-		// Then 이렇게 된다
-		assertThat(event.isFree()).isFalse();
-		
+		assertThat(event.isFree()).isEqualTo(isFree);
 	}
 	
-	@Test
-	public void testOffline() {
+	private Object[] parametersForTestFree() {
+		return new Object[] {
+			new Object[] {0, 0, true},
+			new Object[] {100, 0, false},
+			new Object[] {0, 100, false},
+			new Object[] {100, 200, false}
+		};
+	}
+	
+	@org.junit.Test
+	@Parameters (method = "parametersForTestOffline")
+	public void testOffline(String location, boolean isOffline) {
 		// Given 이런 상태에서
 		Event event = Event.builder()
-				.location("낙성대 5번 출구 앞 빌딩")
+				.location(location)
 				.build();
 		
 		// When 이런일이 벌어지면
@@ -84,16 +73,14 @@ public class EventTest {
 		
 		
 		// Then 이렇게 된다
-		assertThat(event.isOffline()).isTrue();
-		
-		// Given 이런 상태에서
-		event = Event.builder()
-				.build();
-		
-		// When 이런일이 벌어지면
-		event.update();
-		
-		// Then 이렇게 된다
-		assertThat(event.isOffline()).isFalse();
+		assertThat(event.isOffline()).isEqualTo(isOffline);
+	}
+	
+	private Object[] parametersForTestOffline() {
+		return new Object[] {
+			new Object[] {"강남",  true},
+			new Object[] {null, false},
+			new Object[] {"    ", false},
+		};
 	}
 }
