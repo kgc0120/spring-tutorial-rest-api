13f9fa12e71a27f6330027cb7819b587f73a419a
kgc0120 <senru0508@gmail.com>
2020-12-01 23:06:38 +0900

    spring Security Config Setting

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,13 @@
 package com.example.demo;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-	
-	//modelMapper bean으로 등록
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
 	}
 
 }
---
+++ src/main/java/com/example/demo/configs/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.example.demo.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+import com.example.demo.accounts.AccountService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+// WebSecurityConfigurerAdapter 상속 받는 순간 spring boot가 제공해주는 스프링 Security 설정은 더이상 적용되지 않음
+	
+	@Autowired
+	AccountService accountService;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Bean 
+	// token을 저장하는 곳, 토큰 저장소
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(accountService)
+				.passwordEncoder(passwordEncoder);
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().mvcMatchers("/docs/index.html");
+		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+	}
+
+	@Override
+	// Spring Security 안으로는 들어온다.
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.mvcMatchers("/docs/index.html").anonymous()
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).anonymous();
+	}
+
+	
+	
+	
+}
